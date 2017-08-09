ed879fd894de61ddf525179219d73b0cc5792f56
sjacobs146 <sjacobs146@gmail.com>
2017-08-09 14:34:44 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -31,7 +31,10 @@ var checkForMatch = function() {
 		alert("Sorry, try again");
 	}
 }
-var flipCard = function(cardId) {
+var flipCard = function() {
+
+	var cardId = this.getAttribute('data-id');
+	console.log('CardId: ' + cardId);
 
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
@@ -39,10 +42,19 @@ var flipCard = function(cardId) {
 
 	cardsInPlay.push(cards[cardId].rank);
 
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 
 }
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
